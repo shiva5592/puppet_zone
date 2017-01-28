2f747fe6c6da48a59c693c1f27f91c8df042b7f4
root <root@ip-172-31-19-248.ap-south-1.compute.internal>
2017-01-28 11:38:34 +0000

    modified apache2

--- apache/apache.pp
+++ apache/apache.pp
@@ -1,9 +1,9 @@
-package { 'httpd':
+package { 'apache2':
     ensure => installed,
     
 }
 
-service { "httpd":
+service { "apache2":
     ensure     => running,
     enable     => true,
     hasrestart => true,
